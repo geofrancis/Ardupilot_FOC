4b8bc9c2aa8ca8e00b9b5481a21ad6c714db140d
geofrancis <o.o.o.o@hotmail.co.uk>
2026-05-29 17:37:57 +0100

    Refactor motor control for dual motor setup

--- SimpleFOC.ino
+++ SimpleFOC.ino
@@ -5,56 +5,71 @@
 #include <esp_task_wdt.h>
 TaskHandle_t motorTaskHandle;
 
+#define ESC1
+//#define ESC2
+//#define ESC3
 
 int DZ = 20;  // dead
-int MAXRPM = 200;
-float target_velocity = 1;
+float MAXRPM = 10;
+float target_velocity = 0;
 
+float Vlimit = 3;
+
+
+#ifdef ESC1
 int ESC = 140;  //board number
-//int ESC = 141;  //board number
-//int ESC = 142;  //board number
+
+HallSensor sensor1 = HallSensor(18, 19, 13, 15);  
+HallSensor sensor = HallSensor(23, 5, 15, 15);    
+
+BLDCMotor motor = BLDCMotor(15);
+BLDCDriver3PWM driver = BLDCDriver3PWM(32, 25, 33, 15);
+
+BLDCMotor motor1 = BLDCMotor(15);
+BLDCDriver3PWM driver1 = BLDCDriver3PWM(26, 27, 14, 15);
+#endif
+
+#ifdef ESC2
+int ESC = 141;  //board number
+HallSensor sensor = HallSensor(18, 15, 19, 15);
+HallSensor sensor1 = HallSensor(5, 23, 13, 15);
+BLDCMotor motor = BLDCMotor(15);
+BLDCDriver3PWM driver = BLDCDriver3PWM(32, 33, 25, 15);
+BLDCMotor motor1 = BLDCMotor(15);
+BLDCDriver3PWM driver1 = BLDCDriver3PWM(26, 27, 14, 15);
+#endif
+
+#ifdef ESC3
+int ESC = 142;  //board number
+HallSensor sensor1 = HallSensor(15, 18, 19, 15);
+HallSensor sensor = HallSensor(23, 5, 13, 15);
+BLDCMotor motor = BLDCMotor(15);
+BLDCDriver3PWM driver1 = BLDCDriver3PWM(32, 33, 25, 15);
+BLDCMotor motor1 = BLDCMotor(15);
+BLDCDriver3PWM driver = BLDCDriver3PWM(27, 26, 14, 15);
+#endif
 
 
-float Vlimit = 1.5;
 
-
-
-
-
-HallSensor sensorL = HallSensor(18, 15, 19, 15);
-
-void doAL() {
-  sensorL.handleA();
+void doA() {
+  sensor.handleA();
 }
-void doBL() {
-  sensorL.handleB();
+void doB() {
+  sensor.handleB();
 }
-void doCL() {
-  sensorL.handleC();
-}
-
-HallSensor sensorR = HallSensor(5, 23, 13, 15);
-void doAR() {
-  sensorR.handleA();
-}
-void doBR() {
-  sensorR.handleB();
-}
-void doCR() {
-  sensorR.handleC();
+void doC() {
+  sensor.handleC();
 }
 
-
-BLDCMotor motorL = BLDCMotor(15);
-BLDCDriver3PWM driverL = BLDCDriver3PWM(32, 33, 25, 15);
-
-BLDCMotor motorR = BLDCMotor(15);
-BLDCDriver3PWM driverR = BLDCDriver3PWM(26, 27, 14, 15);
-
-
-
-InlineCurrentSense current_senseL = InlineCurrentSense(0.01f, 50.0f, 39, 36);
-InlineCurrentSense current_senseR = InlineCurrentSense(0.01f, 50.0f, 35, 34);
+void doA1() {
+  sensor1.handleA();
+}
+void doB1() {
+  sensor1.handleB();
+}
+void doC1() {
+  sensor1.handleC();
+}
 
 
 
@@ -65,8 +80,8 @@ int rightoutputraw = 1500;
 unsigned long previousMillis = 0;
 const long telem = 2000;
 
-int leftoutput = 0;
-int rightoutput = 0;
+float leftoutput = 0;
+float rightoutput = 0;
 
 int DI1O = 0;
 int FCHB = 0;
@@ -86,18 +101,6 @@ float velocityR = 0;
 float voltageqR = 0;
 float currentqR = 0;
 
-float lca;
-float lcb;
-float lcc;
-float lc;
-
-float rca;
-float rcb;
-float rcc;
-float rc;
-
-
-
 
 uint8_t system_id = 1;
 uint8_t component_id = 158;
@@ -107,23 +110,36 @@ uint8_t chunk_seq = 0;
 
 
 
-void core0Task(void* pvParameters) {
+void Left_task(void* pvParameters) {
   FOC_SETUPL();
+  for (;;) {
+    // noInterrupts();
+    motor.loopFOC();
+    // interrupts();
+    // vTaskDelay(1);
+    motor.move(3);
+  }
+}
+
+
+void Right_task(void* pvParameters) {
   FOC_SETUPR();
   for (;;) {
-    motorL.loopFOC();
-    motorR.loopFOC();
+    // noInterrupts();
+    motor1.loopFOC();
+    // interrupts();
+    //vTaskDelay(1);
+    motor1.move(3);
   }
 }
 
 
 
 
-
 void setup() {
   Serial.begin(115200);  //Main serial port for console output
-  Serial1.begin(500000, SERIAL_8N1, 17, 16);
-  Serial2.begin(500000, SERIAL_8N1, 2, 4);  //GPS+AIS
+  Serial1.begin(230400, SERIAL_8N1, 16, 17);
+  //Serial2.begin(38400, SERIAL_8N1, 2, 4);  //GPS+AIS
 
   esp_task_wdt_config_t twdt_config = {
     .timeout_ms = 5000,
@@ -132,16 +148,27 @@ void setup() {
   };
   esp_task_wdt_reconfigure(&twdt_config);
 
-  driverL.init();
-  driverR.init();
-  motorL.linkDriver(&driverL);
-  motorR.linkDriver(&driverR);
+  driver.init();
+  driver1.init();
+  motor.linkDriver(&driver);
+  motor1.linkDriver(&driver1);
 
 
 
   xTaskCreatePinnedToCore(
-    core0Task,         // Function to implement the task
-    "MotorTask",       // Name of the task
+    Left_task,         // Function to implement the task
+    "Left Motor",      // Name of the task
+    10000,             // Stack size in words (10k is usually plenty)
+    NULL,              // Task input parameter
+    5,                 // Priority of the task (1 is higher than 0)
+    &motorTaskHandle,  // Task handle
+    0                  // Core where the task should run (Core 0)
+  );
+
+
+  xTaskCreatePinnedToCore(
+    Right_task,        // Function to implement the task
+    "Right Motor",     // Name of the task
     10000,             // Stack size in words (10k is usually plenty)
     NULL,              // Task input parameter
     5,                 // Priority of the task (1 is higher than 0)
@@ -157,16 +184,13 @@ void setup() {
 
 void loop() {
   vTaskDelay(1);
-  if (Serial2.available()) {
-    Serial1.write(Serial2.read());
-  }
-  //motorL.monitor();
-  //motorL.monitor();
+  //motor.monitor();
+  //motor1.monitor();
+  //MavLink_RC();
 
-  MavLink_RC();
-  //FOC_Speed();
-  motorL.move(target_velocity);
-  motorR.move(target_velocity);
+  //motor.move(1);
+  //motor1.move(1);
+
 
 
 
@@ -175,12 +199,12 @@ void loop() {
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= telem) {
     previousMillis = currentMillis;
-    MAVLINK_HB();
+    //MAVLINK_HB();
     // MAVLINK_ESC_1();
 
-    if (DI1O == 1) { Mavlink_Telemetry(); }
-    if (DI1O == 2) { FCHBC(); }
-    if (DI1O == 3) { FOC_telemetry(); }
+    // if (DI1O == 1) { Mavlink_Telemetry(); }
+    //if (DI1O == 2) { FCHBC(); }
+    // if (DI1O == 3) { FOC_telemetry(); }
     // if (DI1O == 4) { sleepcheck(); }
     DI1O++;
     if (DI1O > 4) { DI1O = 1; }
